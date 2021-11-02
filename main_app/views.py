dd38b467d7ea6ea1c33282e7fba1072e346fc282
zach-ceneviva <zceneviva@gmail.com>
2021-11-02 15:24:56 -0500

    Added forms.py

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic.base import TemplateView
 from django.views.generic import DeleteView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views import View
 from django.urls import reverse
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
@@ -9,6 +10,7 @@ from django.contrib.auth.models import User
 from .models import City, Profile, Event, Post
 from django.db.models import Count, Q
 from django.http import HttpResponse
+from django import forms
 
 # Create your views here.
 class Home(TemplateView):
@@ -19,3 +21,20 @@ class ProfileView(TemplateView):
 
 class CityDetailView(TemplateView):
     template_name='city_view.html'
+
+
+class Signup(View):
+    def get(self, request):
+        form = UserCreationForm()
+        context = {"form": form}
+        return render (request, "registration/signup.html", context)
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect ('restaurants')
+        else:
+            context = {"form": form}
+            return render(request, 'registration/signup.html', context)
