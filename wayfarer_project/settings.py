b74e4a376107d291a99c4467f567be38b98aa68c
fnedarb <braden.fink49@gmail.com>
2021-11-03 19:13:20 -0400

    Adds heroku deployment config

--- wayfarer_project/settings.py
+++ wayfarer_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,3 +137,5 @@ STATICFILES_DIRS = [
 
 LOGIN_REDIRECT_URL = '/profile/'
 LOGOUT_REDIRECT_URL = '/'
+
+django_heroku.settings(locals())
