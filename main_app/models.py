4e3c478ec749a4ab501e2b72dfd2832288b4286c
zach-ceneviva <zceneviva@gmail.com>
2021-11-02 23:09:09 -0500

    Updated models and profile view

--- main_app/models.py
+++ main_app/models.py
@@ -41,7 +41,7 @@ class Event(models.Model):
     venue = models.CharField(max_length = 200)
     description = models.TextField(max_length = 5000)
     address = models.TextField(max_length = 2000)
-    users_attending = models.ManyToManyField(Profile)
+    users_attending = models.ManyToManyField(User, null=True)
     image = models.URLField(max_length = 300, null=True)
 
     def __str__(self):
@@ -50,7 +50,7 @@ class Event(models.Model):
 
 
 class Post(models.Model):
-    profile = models.ForeignKey(Profile, on_delete = models.CASCADE)
+    profile = models.ForeignKey(User, on_delete = models.CASCADE)
     title = models.CharField(max_length = 1000)
     city = models.ForeignKey(City, on_delete = models.CASCADE, related_name = "post_city")
     date = models.DateTimeField(auto_now_add=True)
--- main_app/views.py
+++ main_app/views.py
@@ -30,13 +30,14 @@ class Home(TemplateView):
 
 #@method_decorator(login_required, name='dispatch')
 class ProfileView(TemplateView):
-    model = Profile
     template_name='profile.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["events"] = Event.objects.filter(users_attending=self.request.user)
-        context["post"] = Post.objects.filter(user=self.request.user)
+        context["events"] = Event.objects.filter(users_attending__id=self.request.user.id)
+        context["post"] = Post.objects.filter(id=self.request.user.id)
+        context["profile"] = Profile.objects.filter(user=self.request.user)
+        return context
 
 class CityDetailView(TemplateView):
     model = City
